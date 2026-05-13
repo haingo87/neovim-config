3d10d8bbeb53d3f80ca44628d7e9958bbf17cae9
Hai Ngo <haiqh2006ic@gmail.com>
2026-05-13 17:27:23 +0700

    feat: auto-open neo-tree on startup when .project is detected

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,15 @@ vim.g.maplocalleader = " "
 vim.g.initial_cwd = vim.fn.getcwd()
 require("util.project").detect(vim.g.initial_cwd)
 
+if vim.g.project then
+	vim.api.nvim_create_autocmd("UIEnter", {
+		once = true,
+		callback = function()
+			vim.cmd("Neotree show")
+		end,
+	})
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
