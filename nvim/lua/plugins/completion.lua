4271010373a0c18ebf2fd5e836a6ceaac85e4784
Hai Ngo <haiqh2006ic@gmail.com>
2026-05-13 12:42:39 +0700

    feat: add completion stack (copilot, nvim-cmp, LuaSnip)

---
+++ nvim/lua/plugins/completion.lua
@@ -0,0 +1,108 @@
+return {
+	{
+		"L3MON4D3/LuaSnip",
+		event = "InsertEnter",
+		dependencies = { "saadparwaiz1/cmp_luasnip" },
+		config = function()
+			require("luasnip.loaders.from_lua").lazy_load()
+		end,
+	},
+
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					keymap = {
+						accept = "<Tab>",
+						accept_word = false,
+						accept_line = false,
+						next = "<M-]>",
+						prev = "<M-[>",
+						dismiss = "<C-]>",
+					},
+				},
+				panel = { enabled = false },
+				filetypes = {
+					["*"] = true,
+				},
+			})
+		end,
+	},
+
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"zbirenbaum/copilot-cmp",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			local has_words_before = function()
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s")
+						== nil
+			end
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp.mapping(function(fallback)
+						local copilot_suggestion = require("copilot.suggestion")
+						if copilot_suggestion.is_visible() then
+							copilot_suggestion.accept()
+						elseif cmp.visible() then
+							cmp.select_next_item()
+						elseif require("luasnip").expand_or_jumpable() then
+							require("luasnip").expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if require("luasnip").jumpable(-1) then
+							require("luasnip").jump(-1)
+						else
+							cmp.select_prev_item()
+						end
+					end, { "i", "s" }),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<C-e>"] = cmp.mapping.abort(),
+				}),
+				sources = cmp.config.sources(
+					{
+						{ name = "nvim_lsp" },
+						{ name = "luasnip" },
+						{ name = "copilot", group_index = 2 },
+					},
+					{
+						{ name = "buffer" },
+						{ name = "path" },
+					}
+				),
+			})
+		end,
+	},
+}
