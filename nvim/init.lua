c495401247104443032c51d2522a31ecd961a2ea
Hai Ngo <haiqh2006ic@gmail.com>
2026-05-13 12:41:37 +0700

    feat: add init.lua with lazy.nvim bootstrap and project detection

---
+++ nvim/init.lua
@@ -0,0 +1,54 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require("util.project").detect(vim.fn.getcwd())
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+
+require("lazy").setup("plugins", {
+	change_detection = { notify = false },
+})
+
+vim.api.nvim_create_user_command("ProjectReload", function()
+	local project = require("util.project")
+	project.cache = {}
+	project.detect(vim.fn.getcwd())
+	project.ensure_lsp_servers()
+	project.ensure_dap_adapters()
+
+	local lspconfig = require("lspconfig")
+	local capabilities = require("cmp_nvim_lsp").default_capabilities(
+		vim.lsp.protocol.make_client_capabilities()
+	)
+
+	if vim.g.project and vim.g.project.env and vim.g.project.env.type == "cpp" then
+		for _, client in pairs(vim.lsp.get_clients({ name = "clangd" })) do
+			client.stop()
+		end
+		lspconfig.clangd.setup({
+			capabilities = capabilities,
+			cmd = { "clangd", "--background-index", "--clang-tidy", "--completion-style=detailed" },
+			init_options = { usePlaceholders = true, completeUnimported = true, clangdFileStatus = true },
+		})
+	end
+
+	vim.cmd("Lazy reload seblj/roslyn.nvim")
+	vim.cmd("Lazy reload stevearc/conform.nvim")
+	vim.cmd("Lazy reload mfussenegger/nvim-dap")
+	vim.notify("[project] Environment reloaded", vim.log.levels.INFO)
+end, {})
